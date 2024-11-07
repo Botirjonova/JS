0600306f4f62d52bf19a875540bfc6e0f3be7e2c
Botirjonova <soliha19837@gmail.com>
2024-11-07 21:42:09 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,16 @@
+let a = prompt('Ism'); // Пользователь вводит имя
+let b = prompt('Familiya'); // Пользователь вводит фамилию
+let c = prompt('Yosh'); // Пользователь вводит возраст
+let d = prompt('Yoqtirgan fan'); // Пользователь вводит любимый предмет
+let p = prompt('Yashash manzili'); // Пользователь вводит адрес
+
+// Теперь мы выводим введенные данные в консоль
+console.log('Ism:', a); // Печатаем 'Ism' и введенное имя
+console.log('Familiya:', b); // Печатаем 'Familiya' и введенную фамилию
+console.log('Yosh:', c); // Печатаем 'Yosh' и введенный возраст
+console.log('Yoqtirgan fan:', d); // Печатаем 'Yoqtirgan fan' и введенный предмет
+console.log('Yashash manzili:', p); // Печатаем 'Yashash manzili' и введенный адрес
+
+
+
+
